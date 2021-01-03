999f6730194e6e549ff81f0cef29a2b02e0d2456
Lukas-Poslusny <48207694+Lukas-Poslusny@users.noreply.github.com>
2021-01-03 18:44:31 +0100

    update #3

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -40,15 +40,14 @@ export class UsersService {
 
   changeUser(id: number, newUsername: string): Observable<IUserEntity> {
     const user: IUserEntity = this.users.find(u => u.id === id);
-    if (user.id === undefined) {
+    if (user === undefined) {
       console.log('No user found.\nid: \'' + id + '\'');
-      return of(user);
     }
     else {
       console.log('Username changed.\nid: \'' + id + '\'\n' + 'username: \'' + user.username + '\' > \'' + newUsername + '\'');
       user.username = newUsername;
-      return of(user);
     }
+    return of(user);
   }
 
 }
